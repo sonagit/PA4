3f725ac9a323a45a82ccf791e444284dea8a9e79
Stu <supadupastu@gmail.com>
2017-05-15 22:24:06 -0700

    user html

--- FilterTweets.java
+++ FilterTweets.java
@@ -13,16 +13,25 @@ public class FilterTweets {
     String htmlStart = "<html>\n<head>\n<meta charset='utf8'>\n <link rel=\"stylesheet\" type=\"text/css\" href=\"tweetstyle.css\">\n</head>\n<body>\n";
     String htmlEnd = "\n</body>\n</html>\n";
 
+    // Ignore calls without options
     if(args.length == 0) {
       System.out.println("Please provide a command: \n");
       System.out.println("java FilterTweets <command> <criterion> <value>\n");
     }
+    
     // Print out HTML
     else if( args.length > 0 && args[0].equals("html") ) {
+      // Print starting HTML
       System.out.println(htmlStart);
-      System.out.println("Hi I'm a wizard");
+      
+      // User queries
+      uq = new UserQuery(args[2]);
+      System.out.println(t.filter(uq).toHTML());
+      
+      // Print end HTML
       System.out.println(htmlEnd);
     }
+    
     // Print out number
     else if(args.length > 0 && args[0].equals("count")) {
       System.out.println("THIS MANY\n*Holds up hands with fingers*");
@@ -33,4 +42,3 @@ public class FilterTweets {
     }
   }
 }
-
